2153e96d639f2a7da8bee4f80869302f60ebaf37
mmjafet <l202123653@jilotepec.tecnm.mx>
2024-12-14 23:01:51 -0600

    empieza el php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+
+    $name = "Jafet";
+    $age = 22;
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=}, initial-scale=1.0">
